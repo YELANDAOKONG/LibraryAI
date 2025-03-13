a8d206d6e3832d57d45e9abb21dda98e22e551fc
YELANDAOKONG <yelandaokong@yldk.xyz>
2025-03-13 13:08:05 +0800

    TEST

--- LibAIDebugger/Program.cs
+++ LibAIDebugger/Program.cs
@@ -1,9 +1,26 @@
-﻿namespace LibAIDebugger;
+﻿using System.ClientModel;
+using Azure.AI.OpenAI;
+using OpenAI;
+using OpenAI.Chat;
+
+namespace LibAIDebugger;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        var options = new OpenAIClientOptions();
+        options.Endpoint = new Uri("https://api.deepseek.com/");
+        var service = new OpenAIClient(
+            new ApiKeyCredential(""),
+            options
+        );
+
+        var chat = service.GetChatClient("deepseek-chat");
+        var data = chat.CompleteChat(new AssistantChatMessage("You are a helpful robot."), new UserChatMessage("Hello, World!"));
+        Console.WriteLine(data.Value.Content[data.Value.Content.Count - 1].Text);
+        
+
     }
 }
