642408174bad48c796e6972c891c2f264f6de3d5
YELANDAOKONG <yelandaokong@yldk.xyz>
2025-03-13 16:16:01 +0800

    Update ChunkTools.cs

---
+++ LibAI/Tools/ChunkTools.cs
@@ -0,0 +1,28 @@
+﻿namespace LibAI.Tools;
+
+public class ChunkTools
+{
+    public Stream InputStream;
+    public int ChunkSize;
+    public int ChunkOverlap;
+    public Action<string>? ChunkCallback;
+    
+    public ChunkTools(
+        Stream input, 
+        int chunkSize = 1024, 
+        int chunkOverlap = 256,
+        Action<string>? chunkCallback = null
+    )
+    {
+        InputStream = input;
+        ChunkSize = chunkSize;
+        ChunkOverlap = chunkOverlap;
+        ChunkCallback = chunkCallback;
+    }
+    
+    public void Chunk()
+    {
+        // TODO...
+    }
+    
+}
