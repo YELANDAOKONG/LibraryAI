c7788d69bd12ecdb4ac8ecad368e351e506bf34b
YELANDAOKONG <yelandaokong@yldk.xyz>
2025-03-13 12:58:08 +0800

    INIT

---
+++ ConsoleAI/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsoleAI;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ LibAIDebugger/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace LibAIDebugger;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
