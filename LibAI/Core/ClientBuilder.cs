2f14cafb9f71f80203d2a254cf79024ec0bf7cd9
YELANDAOKONG <yelandaokong@yldk.xyz>
2025-03-13 16:08:04 +0800

    Update LibAI

---
+++ LibAI/Core/ClientBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System.ClientModel;
+using OpenAI;
+
+namespace LibAI.Core;
+
+public class ClientBuilder
+{
+    public static OpenAIClient Build(string endpoint, string key)
+    {
+        var options = new OpenAIClientOptions();
+        options.Endpoint = new Uri(endpoint);
+        return new OpenAIClient(new ApiKeyCredential(key), options);
+    } 
+}
--- LibAIDebugger/Program.cs
+++ LibAIDebugger/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.ClientModel;
 using Azure.AI.OpenAI;
+using LibAI.Core;
 using OpenAI;
 using OpenAI.Chat;
 
@@ -10,11 +11,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-        var options = new OpenAIClientOptions();
-        options.Endpoint = new Uri("https://api.deepseek.com/");
-        var service = new OpenAIClient(
-            new ApiKeyCredential("sk-"),
-            options
+        var service = ClientBuilder.Build(
+            "https://api.siliconflow.cn/v1/",
+            Environment.GetEnvironmentVariable("KEY")!
         );
 
         var models = service.GetOpenAIModelClient().GetModels();
@@ -24,22 +23,29 @@ class Program
             Console.WriteLine($"{model.Id}, {model.CreatedAt}, {model.OwnedBy}");
         }
         
-        // var chat = service.GetChatClient("deepseek-chat");
+        // var chat = service.GetChatClient("Qwen/Qwen2.5-7B-Instruct");
         // // var data = chat.CompleteChat(new AssistantChatMessage("You are a helpful robot."), new UserChatMessage("Hello, World!"));
         // // Console.WriteLine(data.Value.Content[data.Value.Content.Count - 1].Text);
         // var data = chat.CompleteChatStreaming(
         //     new AssistantChatMessage("You are a helpful robot."),
         //     new UserChatMessage("你好！你是谁？"),
         //     new AssistantChatMessage("你好！我是一个人工智能助手，旨在帮助你解答问题、提供信息或进行对话。你可以问我任何问题，我会尽力提供帮助。有什么我可以为你做的吗？"),
-        //     new UserChatMessage("好的！请帮我写一段代码：\n在Ubuntu 24.04LTS上安装所有LTS JDK版本和JDK 23")
+        //     new UserChatMessage("好的！请帮我写一段代码：\n在Ubuntu 22.04LTS上安装所有LTS JDK版本")
         // );
         //
         // foreach (var update in data)
         // {
-        //     Console.Write(update.ContentUpdate[update.ContentUpdate.Count - 1].Text);
+        //     Console.Write(update.ContentUpdate[^1].Text);
         // }
-        
-        
+
+        // var embed = service.GetEmbeddingClient("BAAI/bge-m3");
+        // var result = embed.GenerateEmbedding("Hello World");
+        // Console.WriteLine(result.Value.Index);
+        // foreach (var data in result.Value.ToFloats().ToArray())
+        // {
+        //     Console.WriteLine(data);
+        // }
+
 
     }
 }
