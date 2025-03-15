bccdef11eec62fd16162ab0407573a53f653022b
YELANDAOKONG <yelandaokong@yldk.xyz>
2025-03-16 07:32:27 +0800

    Update Debugger

--- LibraryAIDebugger/Program.cs
+++ LibraryAIDebugger/Program.cs
@@ -12,38 +12,43 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-        // var service = ClientBuilder.Build(
-        //     "https://api.siliconflow.cn/v1/",
-        //     Environment.GetEnvironmentVariable("KEY")!
-        // );
+        var service = ClientBuilder.Build(
+            "https://api.siliconflow.cn/v1/",
+            Environment.GetEnvironmentVariable("KEY")!
+        );
 
-        // var models = service.GetOpenAIModelClient().GetModels();
-        // Console.WriteLine($"Got Models: {models.Value.Count}");
-        // foreach (var model in models.Value)
-        // {
-        //     Console.WriteLine($"{model.Id}, {model.CreatedAt}, {model.OwnedBy}");
-        // }
+        var models = service.GetOpenAIModelClient().GetModels();
+        Console.WriteLine($"Got Models: {models.Value.Count}");
+        foreach (var model in models.Value)
+        {
+            Console.WriteLine($"{model.Id}, {model.CreatedAt}, {model.OwnedBy}");
+        }
         
         // TestChunkTools();
         // VecDbDemo.Run();
         // ChunkDemo.Run();
         // VecDbLocalDemo.Run();
-        VecDbLocalSearchDemo.Run();
+        // VecDbLocalSearchDemo.Run();
         
-        // var chat = service.GetChatClient("Qwen/Qwen2.5-7B-Instruct");
-        // // var data = chat.CompleteChat(new AssistantChatMessage("You are a helpful robot."), new UserChatMessage("Hello, World!"));
-        // // Console.WriteLine(data.Value.Content[data.Value.Content.Count - 1].Text);
-        // var data = chat.CompleteChatStreaming(
-        //     new AssistantChatMessage("You are a helpful robot."),
-        //     new UserChatMessage("你好！你是谁？"),
-        //     new AssistantChatMessage("你好！我是一个人工智能助手，旨在帮助你解答问题、提供信息或进行对话。你可以问我任何问题，我会尽力提供帮助。有什么我可以为你做的吗？"),
-        //     new UserChatMessage("好的！请帮我写一段代码：\n在Ubuntu 22.04LTS上安装所有LTS JDK版本")
-        // );
-        //
-        // foreach (var update in data)
-        // {
-        //     Console.Write(update.ContentUpdate[^1].Text);
-        // }
+        var chat = service.GetChatClient("deepseek-ai/DeepSeek-V3");
+        // var data = chat.CompleteChat(new AssistantChatMessage("You are a helpful robot."), new UserChatMessage("Hello, World!"));
+        // Console.WriteLine(data.Value.Content[data.Value.Content.Count - 1].Text);
+        var data = chat.CompleteChatStreaming(
+            new AssistantChatMessage("You are a helpful robot."),
+            new UserChatMessage("你好！你是谁？"),
+            new AssistantChatMessage("你好！我是一个人工智能助手，旨在帮助你解答问题、提供信息或进行对话。你可以问我任何问题，我会尽力提供帮助。有什么我可以为你做的吗？"),
+            new UserChatMessage("好的！请帮我写一段代码：\n在Ubuntu 22.04LTS上安装所有LTS JDK版本")
+        );
+        
+        
+        foreach (var update in data)
+        {
+            // Console.Write(update.ContentUpdate.Count);
+            if (update.ContentUpdate.Count >= 1)
+            {
+                Console.Write(update.ContentUpdate[^1].Text);
+            }
+        }
 
         // var embed = service.GetEmbeddingClient("BAAI/bge-m3");
         // var result = embed.GenerateEmbedding("Hello World");
